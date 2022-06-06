79ef483f8a508afec287341ee0d0f793dcf55282
Michał Brotoń <michal.broton.ns@gmail.com>
2022-06-06 20:49:46 +0200

    init

---
+++ pyioga/__init__.py
@@ -0,0 +1,45 @@
+from typing import Optional
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+_SCOPES = ["https://mail.google.com/"]  # Scope for IMAP
+
+_credentials_cache: Optional[Credentials] =  None
+
+def get_access_token(authorized_user_file: str, cache: bool = False) -> str:
+    global _credentials_cache
+    if _credentials_cache is not None and cache:
+        credentials = _credentials_cache
+    else:
+        credentials = get_credentials(authorized_user_file)
+    credentials = ensure_nonexpired(credentials, authorized_user_file=authorized_user_file)
+    if cache:
+        _credentials_cache = credentials
+    return credentials.token
+
+
+def get_credentials(authorized_user_file: str) -> Credentials:
+    return Credentials.from_authorized_user_file(authorized_user_file, _SCOPES)
+
+
+def get_authorized_user_file(client_secret_file: str, output_file: str, port: int = 0) -> None:
+    """Initial"""
+    flow = InstalledAppFlow.from_client_secrets_file(client_secret_file, _SCOPES)
+    credentials = flow.run_local_server(port=port)
+    _save_credentials(credentials, authorized_user_file=output_file)
+
+
+def ensure_nonexpired(credentials: Credentials, authorized_user_file: Optional[str] = None) -> Credentials:
+    if credentials.expired:
+        if not credentials.refresh_token:
+            raise ValueError("Refresh token is not available")
+        credentials.refresh(Request())
+        if authorized_user_file:
+            _save_credentials(credentials, authorized_user_file=authorized_user_file)
+    return credentials
+
+
+def _save_credentials(credentials: Credentials, authorized_user_file: str) -> None:
+    with open(authorized_user_file, "w") as file:
+        file.write(credentials.to_json())
---
+++ pyioga/__main__.py
@@ -0,0 +1,13 @@
+import click
+import pyioga
+
+@click.command()
+@click.option("--client-secret-file", required=True, help="Path to client secrets file, downloaded from GCP/Credentials", type=str)
+@click.option("--output-file", required=True, help="Path in which authorized user file will be created", type=str)
+@click.option("--port", default=0, help="Port used by local server used by Google to perform `Flow`.", type=int)
+def main(client_secret_file: str, output_file: str, port: int) -> None:
+    pyioga.get_authorized_user_file(client_secret_file=client_secret_file, output_file=output_file, port=port)
+
+
+if __name__ == "__main__":
+    main()
